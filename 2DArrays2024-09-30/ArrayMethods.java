ffa80675e5cbe9f46f0278540887c463c61d3c31
agaziiantc <agaziiantc60@stuy.edu>
2024-09-30 22:40:13 -0400

    swapRC

--- 2DArrays2024-09-30/ArrayMethods.java
+++ 2DArrays2024-09-30/ArrayMethods.java
@@ -37,6 +37,24 @@ public class ArrayMethods{
         }
         return c;
     }
+    /**Rotate an array by returning a new array with the rows and columns swapped.
+     * You may assume the array is rectangular and neither rows nor cols is 0.
+     * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+     */
+    public static int[][] swapRC(int[][]nums){
+        int[][] arr = new int[nums[0].length][nums.length];
+        for(int i=0; i < nums[0].length; i++) {
+
+            for (int j = 0; j < nums.length; j++) {
+                arr[i][j] = nums[j][i];
+            }
+        }
+
+
+
+        return arr;
+    }
+
 
     public static void main(String[] args){
         System.out.println("arrayToString([1, 2, 3])" + " -> " + arrayToString(new int[]{1, 2, 3}));
@@ -47,6 +65,9 @@ public class ArrayMethods{
         System.out.println(arr2DSum(arr2) + "| 45");
         System.out.println(arr2DSum(arr3) + "| 0");
         System.out.println(arr2DSum(arr4) + "| 0");
+        System.out.println("swapRC");
+        System.out.println(arr2DToStr(swapRC(new int[][]{{1,2,3},{4,5,6}})) + " | [[1,4],[2,5],[3,6]]");
+
     }
 
 

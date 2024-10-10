dbf48ffe6b24a7db0406eff00abab0006e21cc59
bread1337j <bachakabochuka@gmail.com>
2024-10-10 07:55:11 -0400

    Name and age

---
+++ Driver-10-10/Kitty.java
@@ -0,0 +1,22 @@
+public class Kitty {
+    String name = "Mittens";
+    int age = 2;
+
+
+    public void Kitty(String nam, int ag){
+        name = nam;
+        age = ag;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+
+
+
+}

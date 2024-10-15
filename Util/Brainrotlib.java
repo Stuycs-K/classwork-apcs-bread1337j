4b0ed51e747845c97aa6e79bfcc1ba8e8f0de012
bread1337j <agaziiantc60@stuy.edu>
2024-10-15 08:33:24 -0400

    soon I shall recreate python in Java

--- Util/Brainrotlib.java
+++ Util/Brainrotlib.java
@@ -6,4 +6,5 @@ public class Brainrotlib {
     public static void print(Object obj){ //
         System.out.println(obj);
     }
+    public static int randint(int min, int max){ return(new Random().nextInt(max-min) + min); }
 }

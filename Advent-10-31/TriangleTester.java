f6dd7a8648a80e16ab87961105947100e630be49
bread1337j <agaziiantc60@stuy.edu>
2024-11-04 10:11:34 -0500

    message

--- Advent-10-31/TriangleTester.java
+++ Advent-10-31/TriangleTester.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.*;
 public class TriangleTester{
 	public static void main(String[] args){
-		System.out.println(countTrianglesB("inputTri.txt"));
+		System.out.println(countTrianglesA("inputTri.txt"));
 	}
 	public static boolean testTriangle(String[] nums){
 		return(Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]) > Integer.parseInt(nums[2])
@@ -32,8 +32,12 @@ public class TriangleTester{
 			Scanner sc = new Scanner(in);
 			int c = 0;
 			while(sc.hasNextLine()){
-				String[] nums = sc.nextLine().split(" ");
-				if(testTriangle(nums)) c+=1;
+				String[] nums1 = new String[3];
+				if(sc.hasNext()){nums1[0] = sc.next();}
+				if(sc.hasNext()){nums1[1] = sc.next();}
+				if(sc.hasNext()){nums1[2] = sc.next();}
+
+				if(testTriangle(nums1)) c+=1;
 			}
 			sc.close();	
 			return c;
@@ -52,38 +56,36 @@ public class TriangleTester{
 				//String[] nums1 = sc.nextLine().split("  ", 0);
 				//String[] nums2 = sc.nextLine().split("  ", 0);
 				//String[] nums3 = sc.nextLine().split("  ", 0); //assume rows are multiples of 3
-				int[] nums1 = new int[3];
-				int[] nums2 = new int[3];
-				int[] nums3 = new int[3]; //the final spaghettification
-
-				nums1[0] = sc.nextInt();
-				nums1[1] = sc.nextInt();
-				nums1[2] = sc.nextInt();
-
-				nums2[0] = sc.nextInt();
-				nums2[1] = sc.nextInt();
-				nums2[2] = sc.nextInt();
+								String[] nums1 = new String[3];
+				String[] nums2 = new String[3];
+				String[] nums3 = new String[3]; //the final spaghettification
+				if(sc.hasNext()){nums1[0] = sc.next();}
+				if(sc.hasNext()){nums1[1] = sc.next();}
+				if(sc.hasNext()){nums1[2] = sc.next();}
 				
-				nums3[0] = sc.nextInt();
-				nums3[1] = sc.nextInt();
-				nums3[2] = sc.nextInt();
-
-
+				if(sc.hasNext()){nums2[0] = sc.next();}
+				if(sc.hasNext()){nums2[1] = sc.next();}
+				if(sc.hasNext()){nums2[2] = sc.next();}
+				
+				if(sc.hasNext()){nums3[0] = sc.next();}
+				if(sc.hasNext()){nums3[1] = sc.next();}
+				if(sc.hasNext()){nums3[2] = sc.next();}
 				//for(int i=0; i<3; i++){
 				//	nums1[i] = nums1[i].trim();
 				//	nums2[i] = nums2[i].trim();
 				//	nums3[i] = nums3[i].trim();
 				//}
 
-
-				
+				String[] tri1 = new String[]{nums1[0], nums2[0], nums3[0]};
+				String[] tri2 = new String[]{nums1[1], nums2[1], nums3[1]};
+				String[] tri3 = new String[]{nums1[2], nums2[2], nums3[2]};
 
 				//System.out.println(Arrays.toString(tri1));
 				//System.out.println(Arrays.toString(tri2));
 				//System.out.println(Arrays.toString(tri3));
-				if(testTriangle(new int[]{nums1[0], nums2[0], nums3[0]})) c += 1;
-				if(testTriangle(new int[]{nums1[1], nums2[1], nums3[1]})) c += 1;
-				if(testTriangle(new int[]{nums1[2], nums2[2], nums3[2]})) c += 1;
+				if(testTriangle(tri1)) c += 1;
+				if(testTriangle(tri2)) c += 1;
+				if(testTriangle(tri3)) c += 1;
 
 			}
 			sc.close();	

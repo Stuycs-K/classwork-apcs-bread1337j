b274ccf29e363a4d314c5f8232050582b6d31886
bread1337j <agaziiantc60@stuy.edu>
2024-11-04 09:55:39 -0500

    works with the big input case now

--- Advent-10-31/TriangleTester.java
+++ Advent-10-31/TriangleTester.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.*;
 public class TriangleTester{
 	public static void main(String[] args){
-		System.out.println(countTrianglesB("InputB.txt"));
+		System.out.println(countTrianglesB("inputTri.txt"));
 	}
 	public static boolean testTriangle(String[] nums){
 		return(Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]) > Integer.parseInt(nums[2])
@@ -41,17 +41,41 @@ public class TriangleTester{
 			Scanner sc = new Scanner(in);
 			int c = 0;
 			while(sc.hasNextLine()){
-				String[] nums1 = sc.nextLine().split(" ");
-				String[] nums2 = sc.nextLine().split(" ");
-				String[] nums3 = sc.nextLine().split(" "); //assume rows are multiples of 3
+				//String[] nums1 = sc.nextLine().split("  ", 0);
+				//String[] nums2 = sc.nextLine().split("  ", 0);
+				//String[] nums3 = sc.nextLine().split("  ", 0); //assume rows are multiples of 3
+				String[] nums1 = new String[3];
+				String[] nums2 = new String[3];
+				String[] nums3 = new String[3]; //the final spaghettification
+
+				nums1[0] = sc.next();
+				nums1[1] = sc.next();
+				nums1[2] = sc.next();
+
+				nums2[0] = sc.next();
+				nums2[1] = sc.next();
+				nums2[2] = sc.next();
+				
+				nums3[0] = sc.next();
+				nums3[1] = sc.next();
+				nums3[2] = sc.next();
+
+
+
+				//for(int i=0; i<3; i++){
+				//	nums1[i] = nums1[i].trim();
+				//	nums2[i] = nums2[i].trim();
+				//	nums3[i] = nums3[i].trim();
+				//}
+
 
 				String[] tri1 = new String[]{nums1[0], nums2[0], nums3[0]};
 				String[] tri2 = new String[]{nums1[1], nums2[1], nums3[1]};
 				String[] tri3 = new String[]{nums1[2], nums2[2], nums3[2]};
 
-				System.out.println(Arrays.toString(tri1));
-				System.out.println(Arrays.toString(tri2));
-				System.out.println(Arrays.toString(tri3));
+				//System.out.println(Arrays.toString(tri1));
+				//System.out.println(Arrays.toString(tri2));
+				//System.out.println(Arrays.toString(tri3));
 				if(testTriangle(tri1)) c += 1;
 				if(testTriangle(tri2)) c += 1;
 				if(testTriangle(tri3)) c += 1;

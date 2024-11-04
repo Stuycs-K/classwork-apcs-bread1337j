554497082a752a52afc267e4fdace456a8e5f084
bread1337j <agaziiantc60@stuy.edu>
2024-11-04 10:01:51 -0500

    final commit for now

--- Advent-10-31/TriangleTester.java
+++ Advent-10-31/TriangleTester.java
@@ -14,6 +14,14 @@ public class TriangleTester{
 			Integer.parseInt(nums[0]) + Integer.parseInt(nums[2]) > Integer.parseInt(nums[1])
 		      );
 	}
+	public static boolean testTriangle(int[] nums){
+		return((nums[0]) + (nums[1]) > (nums[2])
+				&&
+			(nums[1]) + (nums[2]) > (nums[0])
+				&&
+			(nums[0]) + (nums[2]) > (nums[1])
+		      );
+	}
 
 
 
@@ -44,22 +52,21 @@ public class TriangleTester{
 				//String[] nums1 = sc.nextLine().split("  ", 0);
 				//String[] nums2 = sc.nextLine().split("  ", 0);
 				//String[] nums3 = sc.nextLine().split("  ", 0); //assume rows are multiples of 3
-				String[] nums1 = new String[3];
-				String[] nums2 = new String[3];
-				String[] nums3 = new String[3]; //the final spaghettification
+				int[] nums1 = new int[3];
+				int[] nums2 = new int[3];
+				int[] nums3 = new int[3]; //the final spaghettification
 
-				nums1[0] = sc.next();
-				nums1[1] = sc.next();
-				nums1[2] = sc.next();
+				nums1[0] = sc.nextInt();
+				nums1[1] = sc.nextInt();
+				nums1[2] = sc.nextInt();
 
-				nums2[0] = sc.next();
-				nums2[1] = sc.next();
-				nums2[2] = sc.next();
+				nums2[0] = sc.nextInt();
+				nums2[1] = sc.nextInt();
+				nums2[2] = sc.nextInt();
 				
-				nums3[0] = sc.next();
-				nums3[1] = sc.next();
-				nums3[2] = sc.next();
-
+				nums3[0] = sc.nextInt();
+				nums3[1] = sc.nextInt();
+				nums3[2] = sc.nextInt();
 
 
 				//for(int i=0; i<3; i++){
@@ -69,16 +76,14 @@ public class TriangleTester{
 				//}
 
 
-				String[] tri1 = new String[]{nums1[0], nums2[0], nums3[0]};
-				String[] tri2 = new String[]{nums1[1], nums2[1], nums3[1]};
-				String[] tri3 = new String[]{nums1[2], nums2[2], nums3[2]};
+				
 
 				//System.out.println(Arrays.toString(tri1));
 				//System.out.println(Arrays.toString(tri2));
 				//System.out.println(Arrays.toString(tri3));
-				if(testTriangle(tri1)) c += 1;
-				if(testTriangle(tri2)) c += 1;
-				if(testTriangle(tri3)) c += 1;
+				if(testTriangle(new int[]{nums1[0], nums2[0], nums3[0]})) c += 1;
+				if(testTriangle(new int[]{nums1[1], nums2[1], nums3[1]})) c += 1;
+				if(testTriangle(new int[]{nums1[2], nums2[2], nums3[2]})) c += 1;
 
 			}
 			sc.close();	

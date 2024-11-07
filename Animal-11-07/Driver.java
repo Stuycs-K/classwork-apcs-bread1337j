89e9550b0c5ceec32fadd8162cdd818225e08067
bread1337j <agaziiantc60@stuy.edu>
2024-11-07 09:49:36 -0500

    Initial commit

---
+++ Animal-11-07/Driver.java
@@ -0,0 +1,16 @@
+import Util.*;
+
+public class Driver{
+
+	public static void main(String[] args){
+	
+		Animal bobrik = new Animal("AAAAAAAAAAAAAAAAAA", 500, "Bobrik");
+
+		bobrik.speak();
+
+	
+	}
+
+}
+
+

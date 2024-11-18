fe3bd4b5446e03bccedabeeabd06fa9b0e2caf06
agaziiantc <agaziiantc60@stuy.edu>
2024-11-17 22:02:47 -0500

    Test cases

--- 2024-11-14-ArrayList/src/Main.java
+++ 2024-11-14-ArrayList/src/Main.java
@@ -2,30 +2,62 @@ package src;
 
 import src.Util.JohnTimer;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main{
 	public static void main(String[] args){
 		JohnTimer timer = new JohnTimer();
-		System.out.println("Timing creation of array of length 20000000, in ms");
+		System.out.println("Timing creation of array of length 50000000, in ms");
 		timer.start();
-		createRandomArray(20000000);
+		ArrayList<String> longarr = createRandomArray(50000000);
 		timer.record();
 		timer.displayms();
 		System.out.println("Doing it a few more times for good measure");
-		for(int i=0; i<4; i++){
-			createRandomArray(20000000);
+		for(int i=0; i<10; i++){
+			createRandomArray(50000000);
 			timer.record();
 			timer.displayms();
 		}
-	}
 
+		System.out.println("Making random array of length 100:");
+		ArrayList<String> randarr = createRandomArray(100);
+		System.out.println(randarr);
+		System.out.println("Running it through replaceEmpty:");
+		ArrayListPractice.replaceEmpty(randarr);
+		System.out.println(randarr);
+		System.out.println("Running the very long arraylist through replaceEmpty:");
+		timer.start();
+		ArrayListPractice.replaceEmpty(longarr);
+		timer.record();
+		timer.displayms();
+		System.out.println("Reversing the array:");
+		ArrayList<String> reverserandarr = ArrayListPractice.makeReversedList(randarr);
+		System.out.println(reverserandarr);
+		System.out.println("Reversing the very long array: ");
+		timer.start();
+		ArrayList<String> reversedlongarr = ArrayListPractice.makeReversedList(longarr);
+		timer.record();
+		timer.displayms();
+		System.out.println("Mixing array with it's reversed version");
+		ArrayList<String> mix = ArrayListPractice.mixLists(randarr, reverserandarr);
+		System.out.println(mix);
+		System.out.println("Mixing the very long arrays: ");
+		timer.start();
+		ArrayList<String> veryLongMix = ArrayListPractice.mixLists(longarr, reversedlongarr);
+		timer.record();
+		timer.displayms();
+
+	}
+	static SplittableRandom rand = new SplittableRandom(); //faster math.random()
 	static String[] intarr = new String[]{"", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 	public static ArrayList<String> createRandomArray(int size){	
 		ArrayList<String> arr = new ArrayList<String>(size+1);
 
 		for(int i=0; i<size; i++){
-			arr.add(intarr[(int)(Math.random()*11)]);
+
+			arr.add(intarr[(int)(rand.nextDouble(1)*11)]);
+
 		}
 
 		return arr;

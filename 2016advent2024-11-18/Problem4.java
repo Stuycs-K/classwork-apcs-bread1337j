076ea179141683e77e2c08c6c154595d5ba17ca6
bread1337j <agaziiantc60@stuy.edu>
2024-11-20 10:12:21 -0500

    created files

---
+++ 2016advent2024-11-18/Problem4.java
@@ -0,0 +1,14 @@
+import java.utils.*;
+import java.io.*;
+
+
+public class problem4{
+	//notes for future self: just convert to char and compare that. do NOT make an array of the alphabet
+	
+	public static void main(String[] args){
+		try{
+			Scanner sc = new Scanner(new File("Input4.txt"));
+		}catch(FileNotFoundException e){}
+	
+	}
+}

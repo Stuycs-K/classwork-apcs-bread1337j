b593cb64d594e35966d1fe4d5460e65bb4ab96eb
bread1337j <agaziiantc60@stuy.edu>
2024-11-20 10:07:19 -0500

    Problem 1 part 2 solution

--- 2016advent2024-11-18/Problem1.java
+++ 2016advent2024-11-18/Problem1.java
@@ -2,7 +2,13 @@ import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
+class Intgr{
+	int x;
+	public String toString(){
+		return("" + x);	
+	}
 
+}
 public class Problem1{
 	//static int[] dir = new int[]{0, 1};
 	//static int[] matrix1 = new int[]{0, -1, 1, 0};
@@ -15,7 +21,7 @@ public class Problem1{
 //	}
 	static int dir = 0;
 	static int[][] dirs = new int[][]{{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-	static ArrayList<Integer[]> memory = new ArrayList<Integer[]>(500);
+	static ArrayList<String> memory = new ArrayList<String>(500);
 	public static int[] getDir(int dir){
 		if(dir<0){
 			dir += 4;
@@ -39,16 +45,16 @@ public class Problem1{
 			while(sc.hasNext()){
 				String s = sc.next().replaceAll(",", "");
 				s.replaceAll(",", "");
-				System.out.println("" + s.charAt(0) + " | " + s.substring(1, s.length()));
+				//System.out.println("" + s.charAt(0) + " | " + s.substring(1, s.length()));
 				int dist = Integer.valueOf(s.substring(1, s.length()));
-				System.out.println(dist);
+				//System.out.println(dist);
 				if(s.charAt(0) == 'L'){
 					dir += 1;
 					//System.out.println("L");
 					for(int i=0; i<dist; i++) {
 						x = x + getDir(dir)[0];
 						y = y + getDir(dir)[1];
-						Integer[] cord = new Integer[]{Integer.valueOf(x), Integer.valueOf(y)};
+						String cord = ("" + x + " | " + y);
 						if(memory.indexOf(cord) == -1){
 							memory.add(cord);
 						}else{
@@ -59,11 +65,14 @@ public class Problem1{
 					}
 				}else if(s.charAt(0) == 'R') {
 					dir -= 1;
-					System.out.println("R, " + dist + " | " + Arrays.toString(getDir(dir)));
+					//System.out.println("R, " + dist + " | " + Arrays.toString(getDir(dir)));
 					for(int i=0; i<dist; i++) {
 						x = x + getDir(dir)[0];
 						y = y + getDir(dir)[1];
-						Integer[] cord = new Integer[]{Integer.valueOf(x), Integer.valueOf(y)};
+						//System.out.println(x + ", " + y);
+						System.out.println((memory.toString()));
+						String cord = ("" + x + " | " + y);
+						System.out.println(cord);
 						if(memory.indexOf(cord) == -1){
 							memory.add(cord);
 						}else{
@@ -74,9 +83,9 @@ public class Problem1{
 					}
 				}
 				else{
-					System.out.println(s + " " + s.charAt(0) + " " + s.charAt(1));
+					//System.out.println(s + " " + s.charAt(0) + " " + s.charAt(1));
 				}
-				System.out.println("" + x + " , " + y + " | " + getDir(dir)[0] + " , " + getDir(dir)[1] + " ; " + dir + " Total dist so far: " + (Math.abs(x) + Math.abs(y)));
+				//System.out.println("" + x + " , " + y + " | " + getDir(dir)[0] + " , " + getDir(dir)[1] + " ; " + dir + " Total dist so far: " + (Math.abs(x) + Math.abs(y)));
 		
 			}
 			System.out.println(Math.abs(x)+Math.abs(y));

1a27ae6a49fc37521ef3f5b33f22d0f0d7dc233e
bread1337j <agaziiantc60@stuy.edu>
2024-12-03 10:06:05 -0500

    end of class commit

--- ColorDemo.java
+++ ColorDemo.java
@@ -4,9 +4,9 @@ public class ColorDemo{
 	public static void main(String[] args){
 		for(int i=0; i<256; i++){	
 		System.out.println("\u001b[H");	
-		for(int r = i; r < 256; r+=32){
-  			for(int g = i; g <= 256; g+=32){
-    				for(int b = i; b <= 256; b+=32){
+		for(int r = i; r < 256; r+=1){
+  			for(int g = i; g <= 256; g+=1){
+    				for(int b = i; b <= 256; b+=1){
       					System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
     					try{TimeUnit.MILLISECONDS.sleep(1);}catch(Exception e){}
 				}

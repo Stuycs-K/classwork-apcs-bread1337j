0d5be7b48df0274b93f3b5f5d710cb40957146a9
bread1337j <agaziiantc60@stuy.edu>
2024-12-04 09:47:07 -0500

    Update ColorDemo.java

--- ColorDemo.java
+++ ColorDemo.java
@@ -7,9 +7,9 @@ public class ColorDemo{
 		for(int r = i; r < 256; r+=1){
   			for(int g = i; g <= 256; g+=1){
     				for(int b = i; b <= 256; b+=1){
-      					System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
-    					try{TimeUnit.MILLISECONDS.sleep(1);}catch(Exception e){}
-				}
+					System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m ");
+				}try{TimeUnit.MILLISECONDS.sleep(10);}catch(Exception e){}
+				
 				//System.out.println("\u001b[H");
   			}
     		System.out.println();

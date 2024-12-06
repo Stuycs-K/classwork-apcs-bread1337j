9a89f5dd26b0791c7de3f12d5930ac73c71a719b
bread1337j <131266168+bread1337j@users.noreply.github.com>
2024-12-06 09:58:21 -0500

    implemented abstract methods

--- Adventurer-2024-12-06/Skibidi.java
+++ Adventurer-2024-12-06/Skibidi.java
@@ -10,9 +10,8 @@ public class Skibidi extends Adventurer{
 		this.maxspecial = maxspecial;
 	}
 
-
 	public String getSpecialName(){
-		return "Skibidi " + this.name;
+		return "Skibidi " + super.getName();
 	}
 
 	public int getSpecial(){
@@ -21,10 +20,42 @@ public class Skibidi extends Adventurer{
 	public void setSpecial(int n){
 		this.special = n;
 	}
+	public void setMaxSpecial(int n){
+		this.maxspecial = n;
+	}
 	public int getSpecialMax(){
 		return this.maxspecial;
 	}
 
+	public String specialAttack(Adventurer other){
+		if(this.getSpecial() >= 100){
+			other.setName("Skibidi " + other.getName());
+			this.setSpecial(this.getSpecial() - 100);
+		}
+		return other.getName();
+	}
+	public String support(Adventurer other){
+		if(this.getSpecial() >= 1){
+			other = null; //free the user from having to deal with my code
+		}
+		return(other + "");
+	}
+	public String support(){
+		//nevermind it went very wrong this = this(name, super.getHP()*100, this.getSpecial()*5, this.getMaxSpecial()*5); //very safe code that has no possible ways of going wrong!!!!!
+		
+		
+		super.setHP(super.getHP()*100);
+		this.setSpecial(this.getSpecial()*5);
+		this.setMaxSpecial(this.getSpecialMax()*5);
+		return(this.getSpecialName());
+	}
+	public String attack(Adventurer other){
+		int damage = 5000-this.getHP() + this.getSpecial() * this.getSpecialMax();
+		other.setHP(other.getHP()-damage);
+		this.setHP(this.getHP()-damage);
+		return("Was attacked: " + other.getHP() + "Not attacked: "  + this.getHP());
+	
+	}
 
 
 }

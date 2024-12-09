f4bbd88ed2fb9b4e23872aa9b51a03fa56adbfc1
bread1337j <131266168+bread1337j@users.noreply.github.com>
2024-12-09 10:11:54 -0500

    The industrial revolution and its consequences

--- Adventurer-2024-12-06/Main.java
+++ Adventurer-2024-12-06/Main.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main{
 	public static void main(String[] args){
-	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		Adventurer plr = new Skibidi("" + sc.nextLine(), 10, 100, 100);
+		Adventurer Enemy = new CodeWarrior();
+		while(plr != null && Enemy != null){
+				Random rand = new Random(8800);
+				System.out.println(plr.getName() + " | HP: " + plr.getHP() + " | " + plr.getSpecialName() + ": " + plr.getSpecial() + "/" + plr.getSpecialMax());
+				System.out.println(Enemy.getName() + " | HP: " + Enemy.getHP() + " | " + Enemy.getSpecialName() + ": " + Enemy.getSpecial() + "/" + Enemy.getSpecialMax());
+
+				System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+				String in = sc.nextLine() + "";
+				if(in.equals("a") || in.equals("attack")){
+					System.out.println(plr.attack(Enemy));
+				}else if (in.equals("sp") || in.equals("special")) {
+					System.out.println(plr.specialAttack(Enemy));
+				}else if (in.equals("su") || in.equals("support")) {
+					System.out.println(plr.support());
+				}else if (in.equals("quit")){
+					System.out.println("Quitting");
+					System.exit(1);
+				}else{
+					System.out.println("Invalid input, you die of terminal dyslexia.");
+					plr = null;
+					continue;
+				}
+				if(Enemy.getHP() < 0){
+					Enemy = null;
+					continue;
+				}
+				if(plr.getHP() < 0){
+					plr = null;
+					continue;
+				}
+
+				if(in.equals("1")){
+					System.out.println(Enemy.attack(plr));
+				}else if (in.equals("2")) {
+					System.out.println(Enemy.specialAttack(plr));
+				}else if (in.equals("3")) {
+					System.out.println(Enemy.support());
+				}
+				if(Enemy.getHP() < 0){
+					Enemy = null;
+					continue;
+				}
+				if(plr.getHP() < 0){
+					if(plr.getHP() < -10000000){
+						System.out.println("integer overflow moment");
+					}
+					plr = null;
+					continue;
+				}
+		}
+		System.out.println((plr == null)? "you lose":"you win");
 	}
 }
--- Adventurer-2024-12-06/Skibidi.java
+++ Adventurer-2024-12-06/Skibidi.java
@@ -30,31 +30,34 @@ public class Skibidi extends Adventurer{
 	public String specialAttack(Adventurer other){
 		if(this.getSpecial() >= 100){
 			other.setName("Skibidi " + other.getName());
+			other.setSpecial(0);
 			this.setSpecial(this.getSpecial() - 100);
 		}
-		return other.getName();
+		return other.getName() + " gets 360 no-scoped so hard that " + other.getName() + " is brought down to " + other.getSpecial() + " special";
 	}
 	public String support(Adventurer other){
+		String real = other + " is freed";
 		if(this.getSpecial() >= 1){
 			other = null; //free the user from having to deal with my code
 		}
-		return(other + "");
+		return(real);
 	}
 	public String support(){
 		//nevermind it went very wrong this = this(name, super.getHP()*100, this.getSpecial()*5, this.getMaxSpecial()*5); //very safe code that has no possible ways of going wrong!!!!!
-		
-		
+
+
 		super.setHP(super.getHP()*100);
 		this.setSpecial(this.getSpecial()*5);
 		this.setMaxSpecial(this.getSpecialMax()*5);
-		return(this.getSpecialName());
+		return(this.getName() + " got real");
 	}
 	public String attack(Adventurer other){
-		int damage = 5000-this.getHP() + this.getSpecial() * this.getSpecialMax();
-		other.applyDamage(damage);
-		this.applyDamage(damage);
-		return("Was attacked: " + other.getHP() + "Not attacked: "  + this.getHP());
-	
+		int damage = (5000-this.getHP()) * (this.getSpecial() / this.getSpecialMax()); //This will not be documented because not even I know what it does.
+		other.applyDamage(damage + 5);
+		if(this.getHP() < damage){damage = this.getHP()-5;}
+		this.applyDamage(Math.abs(damage)); //game design is my passion
+		return("Was attacked: " + other.getHP() + " Not attacked: "  + this.getHP() + " Info about the attack: No one knows");
+
 	}
 
 
